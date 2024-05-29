75db60e6d8e6eb66dd2fb3cfc6e2d186cb2f5abd
Kevin <kevinrenfc@gmail.com>
2024-05-29 01:28:36 -0700

    added options text for future interactable button

--- Assets/Scripts/Narration/NarrationLine.cs
+++ Assets/Scripts/Narration/NarrationLine.cs
@@ -11,18 +11,21 @@ public class NarrationLine : MonoBehaviour
 
     public GameObject gameObj;
     public TMP_Text text_line;
+    public TMP_Text options_line;
     public NarrationNode narration_line;
 
     // Start is called before the first frame update
 
     void Start()
     {
-        text_line.fontSize = 50;
+        text_line.fontSize = 25;
+        options_line.fontSize = 15;
     }
 
     // Update is called once per frame
     void Update()
     {
         text_line.text = narration_line.line;
+        options_line.text = narration_line.choice_lines[0];
     }
 }
