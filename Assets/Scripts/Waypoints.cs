c248c096bad1040e3cc7201808de0698f469c27a
Bryce <brycewang@ucsb.edu>
2024-05-29 18:34:53 -0700

    running and walking functions

--- Assets/Scripts/Waypoints.cs
+++ Assets/Scripts/Waypoints.cs
@@ -5,29 +5,70 @@ using UnityEngine;
 public class Waypoints : MonoBehaviour
 {
     public GameObject[] waypoints;
-    int current = 0;
-    float rotSpeed;
-    public float speed;
-    float WPradius = 1;
+    private int current = 0;
+    private float rotSpeed;
+    public float walkSpeed = 2f;
+    public float runSpeed = 6f; // Speed for running
+    private float speed;
+    private float WPradius = 1;
 
-    // Start is called before the first frame update
+    private Animator animator;
+
+    // void OnEnable()
+    // {
+    //     Whistle.OnWhistleBlown += StartRunning;
+    // }
+
+    // void OnDisable()
+    // {
+    //     Whistle.OnWhistleBlown -= StartRunning;
+    // }
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        if (animator == null)
+        {
+            Debug.LogError("Animator component not found on the GameObject.");
+        }
+        speed = walkSpeed;    
+        StartRunning();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
+        if (Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
         {
             current++;
-            if(current >= waypoints.Length)
+            if (current >= waypoints.Length)
             {
                 current = 0;
             }
         }
+
         transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
         transform.LookAt(waypoints[current].transform.position);
+
+        UpdateAnimator();
+    }
+
+    void StartRunning()
+    {
+        speed = runSpeed;
+        UpdateAnimator();
+    }
+
+    public void StartWalking()
+    {
+        speed = walkSpeed;
+        UpdateAnimator();
+    }
+
+    void UpdateAnimator()
+    {
+        if (animator != null)
+        {
+            animator.SetFloat("Speed", speed);
+        }
     }
 }
